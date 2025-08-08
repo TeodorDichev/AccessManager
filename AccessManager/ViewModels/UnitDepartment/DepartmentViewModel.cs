baae4e761a42508ba1d4004000ee1c6334603566
Teodor Dichev <teodichev@gmail.com>
2025-08-08 13:35:58 +0300

    Added carousel to main menu and worked on app architecture

---
+++ AccessManager/ViewModels/UnitDepartment/DepartmentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessManager.ViewModels.UnitDepartment
+{
+    public class DepartmentViewModel
+    {
+        public Guid DepartmentId { get; set; }
+        public string DepartmentName { get; set; } = string.Empty;
+        public List<UnitViewModel> Units { get; set; } = [];
+
+    }
+}
---
+++ AccessManager/ViewModels/UnitDepartment/UnitDepartmentListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AccessManager.Data.Enums;
+
+namespace AccessManager.ViewModels.UnitDepartment
+{
+    public class UnitDepartmentListViewModel
+    {
+        public List<DepartmentViewModel> Departments { get; set; } = [];
+        public AuthorityType WriteAuthority { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+    }
+}
